dd936f9f552f0137499b53601aaae9aff595c1c7
Remimidemmi <bizkitstylle@gmail.com>
2023-01-23 15:35:56 +0300

    Test_commit

--- test_main_page.py
+++ test_main_page.py
@@ -1,7 +1,4 @@
-import pytest
-from selenium.webdriver.common.by import By
 from .PageObject.main_page import MainPage
-from .PageObject.base_page import BasePage
 
 
 def test_guest_can_go_to_login_page(browser):
@@ -9,3 +6,9 @@ def test_guest_can_go_to_login_page(browser):
     page = MainPage(browser, link)
     page.open()
     page.go_to_login_page()
+
+def test_guest_should_be_login_link(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    page.should_be_login_link()
