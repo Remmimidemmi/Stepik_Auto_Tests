20dba21acc684ed8c1ce7dd2537f5cbdbc0551f3
Remimidemmi <bizkitstylle@gmail.com>
2023-01-23 13:24:04 +0300

    Test_commit

--- conftest.py
+++ conftest.py
@@ -1,9 +1,7 @@
+import time
 import pytest
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.remote.webelement import WebElement
-import time
+
 
 @pytest.fixture(scope="function")
 def browser():
