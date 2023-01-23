5ba90ace916d483d277e3a323695f6723d451873
Remimidemmi <bizkitstylle@gmail.com>
2023-01-23 12:28:09 +0300

    Test_commit

---
+++ conftest.py
@@ -0,0 +1,35 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.remote.webelement import WebElement
+import time
+
+@pytest.fixture(scope="function")
+def browser():
+    browser = webdriver.Chrome()
+    yield browser
+    time.sleep(10)
+    browser.quit()
+
+
+def pytest_addoption(parser):
+    parser.addoption('--language', action='store', default='en',
+                     help="Choose language: 'en', 'fr', 'ru'")
+
+
+@pytest.fixture(scope="function")
+def language_chs(request, browser):
+    language = request.config.getoption("language")
+    if language == 'en':
+        link_en = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/coders-at-work_207/"
+        yield browser.get(link_en)
+    elif language == 'fr':
+        link_fr = "http://selenium1py.pythonanywhere.com/fr/catalogue/coders-at-work_207/"
+        yield browser.get(link_fr)
+    elif language == 'ru':
+        link_ru = "http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/"
+        yield browser.get(link_ru)
+    else:
+        raise pytest.UsageError("--language should be: 'en', 'fr', or 'ru'")
+
